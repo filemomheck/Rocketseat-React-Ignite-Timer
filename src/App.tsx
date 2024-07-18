383f60c573ac07c1e49fd9f5aadac351ac9b3891
UNASP - Filemom Viel Heck <filemom.heck@unasp.edu.br>
2024-07-18 10:44:31 -0300

    Colors & Fonts and Configure ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { Button } from "./Button";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     // Fragment:
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
