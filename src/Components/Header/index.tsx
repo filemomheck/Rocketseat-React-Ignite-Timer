2822c5afb81f6b93fbe4986e2ba666a01501609b
UNASP - Filemom Viel Heck <filemom.heck@unasp.edu.br>
2024-07-24 17:03:48 -0300

    Create Layout (DefaultLayout) and Header.

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,3 +1,22 @@
+import { HeaderContainer } from "./styles";
+import { Timer, Scroll } from "phosphor-react";
+import logotype from "../../assets/logotype-ignite.svg";
+import { NavLink } from "react-router-dom";
+
+// "title" de <NavLink> é uma recomendacao para acessibilidade.
+
 export function Header() {
-  return <></>;
+  return (
+    <HeaderContainer>
+      <img src={logotype} />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />{" "}
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
 }
